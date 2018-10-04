9a0bfa9d0b6e678efa8e357e8833193809b4953f
DeLandtsheerTijl <tijl.de.landtsheer@student.howest.be>
2018-10-04 12:20:48 +0200

    firstpost

---
+++ NMCT.IoT.MyFirstServices/Models/SomModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NMCT.IoT.MyFirstServices.Models
+{
+    public class SomModel
+    {
+        public int Getal1 { get; set; }
+        public int Getal2 { get; set; }
+
+
+    }
+
+    public class SomResultaat
+    {
+        public int Resultaat { get; set; }
+    }
+}
